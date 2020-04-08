2968934436a8945fe88af34f55a70415f9401b3c
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-04-08 12:02:25 +0300

    Fix articles query count and rows

--- src/graphql/schema/queries/articles.js
+++ src/graphql/schema/queries/articles.js
@@ -4,16 +4,21 @@ const knex = require('../../../services/knex');
 
 const ArticlesListType = require('../types/ArticlesList');
 
+const DEFAULT_LIMIT = 10;
+const MAX_LIMIT = 1000;
+
 module.exports = {
   name: 'Articles',
 
   type: new GraphQLNonNull(ArticlesListType),
 
-  resolve: async () => {
+  resolve: async (root, args) => {
+    const limit = Math.min(MAX_LIMIT, Math.max(+args.limit || DEFAULT_LIMIT, 0));
+
     const builder = knex('articles');
 
-    const count = await builder.count();
-    const rows = await builder.select();
+    const { count } = await builder.clone().count().first();
+    const rows = await builder.select().limit(limit);
 
     return { count, rows };
   },
