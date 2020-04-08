d15e16d5b203cd487e6976e4bfae86d8afcc58bd
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-04-09 00:30:38 +0300

    Add ssl: false to migrator

--- migrator/src/index.js
+++ migrator/src/index.js
@@ -38,6 +38,7 @@ async function applyMigrations() {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
+      ssl: false,
     },
   });
 
